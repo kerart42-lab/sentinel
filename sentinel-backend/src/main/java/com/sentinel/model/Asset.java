e8a332facabf2075cf245a228c0c81b646e2469e
kerart42-lab <kerart42@gmail.com>
2026-07-18 19:40:53 -0700

    Add constructor to Asset class

--- sentinel-backend/src/main/java/com/sentinel/model/Asset.java
+++ sentinel-backend/src/main/java/com/sentinel/model/Asset.java
@@ -8,4 +8,58 @@ public class Asset {
     private String serialNumber;
     private String assignedOwner;
     private String location;
+
+    //Constructors
+    public Asset(String name, String type, AssetStatus status, String serialNumber, String assignedOwner, String location) {
+        this.name = name;
+        this.type = type;
+        this.status = status;
+        this.serialNumber = serialNumber;
+        this.assignedOwner = assignedOwner;
+        this.location = location;
+    }
+    //Getter
+    public Long getId() {
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getType(){
+        return type;
+    }
+    public AssetStatus getStatus(){
+        return status;
+    }
+    public String getSerialNumber(){
+        return serialNumber;
+    }
+    public  String getAssignedOwner(){
+        return assignedOwner;
+    }
+    public String getLocation(){
+        return location;
+    }
+    // Setter
+    public void setId(Long id){
+        this.id = id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    public void setStatus(AssetStatus status){
+        this.status = status;
+    }
+    public void setSerialNumber(String serialNumber){
+        this.serialNumber = serialNumber;
+    }
+    public void setAssignedOwner(String assignedOwner){
+        this.assignedOwner = assignedOwner;
+    }
+    public void setLocation(String location){
+        this.location = location;
+    }
 }
